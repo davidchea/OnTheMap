005d4c99871ae4721faa6dff6346369ddbd7d8c6
David Chea <chea.david.pro@gmail.com>
2019-08-31 11:28:40 +0200

    Sign up link

--- OnTheMap/Main/MainViewController.swift
+++ OnTheMap/Main/MainViewController.swift
@@ -9,8 +9,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var signUpTextView: UITextView!
+    
+    // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setSignUpTextView()
+    }
+    
+    // MARK: Methods
+    
+    /// Set the sign up link and center the text view.
+    func setSignUpTextView() {
+        let attributedString = NSMutableAttributedString(string: signUpTextView.text!)
+        let url = URL(string: "https://auth.udacity.com/sign-up?next=https://classroom.udacity.com/authenticated")!
+    
+        // Set the "Sign up" substring to be the link
+        attributedString.setAttributes([.link: url], range: NSMakeRange(23, 8))
+        
+        signUpTextView.attributedText = attributedString
+        signUpTextView.textAlignment = NSTextAlignment.center
     }
 }
